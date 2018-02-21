8d1723a884f064bed756b3e34bc0cef739586c08
seanvanwyk41 <seanvanwyk41@gmail.com>
2018-02-20 20:56:07 -0500

    Added the files and file structures for a node-restify-postgres server. The template was provided by a public github repo

---
+++ src/config/settings.js
@@ -0,0 +1,12 @@
+export default {
+  name: 'node-restify-postgres',
+  version: '1.0.0',
+  port: 8080,
+  db: {
+    host: 'howlplay-db.czfcpgzgc9ja.us-west-2.rds.amazonaws.com',
+    port: 5432,
+    database: 'howlplay',
+    user: 'howlplay',
+    password: 'FFyKER7XEq5erkGW'
+  },
+};

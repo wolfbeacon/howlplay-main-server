68f4f20fca24ff9dba3c1f7b3fea692b23091f70
seanvanwyk41 <seanvanwyk41@gmail.com>
2018-03-14 09:39:27 -0400

    Added database conection testing

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 //Imports and Requirements
 var restify = require('restify');
 var Sequelize = require('sequelize');
+var uuidv4 = require('uuid/v4');
 
 //Database config
 var env = "dev";
@@ -11,7 +12,7 @@ var password = config.password ? config.password : null;
 var sequelize = new Sequelize(
     config.database,
     config.user,
-    password,
+    config.password,
     {
         dialect: config.dialect,
         port: config.port,
@@ -19,12 +20,28 @@ var sequelize = new Sequelize(
         logging: console.log,
         define: {
             timestamps: false
+        },
+        pool: {
+          max: 5,
+          min: 0,
+          acquire: 30000,
+          idle: 10000
         }
     }
 );
 
+//Authenticate Database connection
+sequelize
+  .authenticate()
+  .then(function(err) {
+    console.log('Connection has been established successfully.');
+  })
+  .catch(function (err) {
+    console.log('Unable to connect to the database:', err);
+  });
+
 //Models
-const quizzes = sequelize.define('quizzes',{
+const Quizzes = sequelize.define('quizzes',{
   name: {type: Sequelize.STRING(512)},
   questions: {type: Sequelize.STRING},
   access_token: {type: Sequelize.STRING(128)}
@@ -32,15 +49,16 @@ const quizzes = sequelize.define('quizzes',{
 });
 
 //Global Constants
-var WIP = "Endpoint not implemented yet\n";
+var WIP = 'Endpoint not implemented yet';
 
-// Start Server
+// Create Server
 var server = restify.createServer();
 
 //Initualize Body Parser
 server.use(restify.plugins.queryParser({ mapParams: true}));
 server.use(restify.plugins.bodyParser({ mapParams: true}));
 
+// Start Server
 server.listen(8080, function() {
   console.log('%s listening at %s', server.name, server.url);
 });
@@ -48,7 +66,10 @@ server.listen(8080, function() {
 // Quiz End Points
 // Creat New Quiz
 server.post('/quiz', function(req, res, next) {
-  console.log(req.params);
+  var name = params.name;
+  var questions = param.question.stringify();
+  var access_token = uuidv4();
+  console.log(req.params.name);
   res.send(200, WIP);
   next();
 });

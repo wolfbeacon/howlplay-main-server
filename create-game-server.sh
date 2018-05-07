6bfd52138e07c2e9fd512d97fda1cad30c917deb
David Wan <dave.wan@mail.utoronto.ca>
2018-05-06 21:07:09 -0400

    Script now retrieves environment variables and runs Docker with them. Docker command isn't correct though

--- create-game-server.sh
+++ create-game-server.sh
@@ -1,4 +1,18 @@
-#!bin/bash
+#!/bin/bash
 
-VARNAME bob = "hello";
-echo bob;
+IFS=':'
+
+if [ $# -eq 0 ]
+then
+  echo 'Usage: ./create-game-server.sh ENV_VAR_0:value_0 ENV_VAR_1:value_1 ... ENV_VAR_N:value_N';
+  exit 1;
+fi
+
+while (("$#"));
+do
+  var=($1)
+  declare "${var[0]}"=${var[1]};
+  shift;
+done
+
+docker run --env QUIZ_HASH --env ADMIN_KEY ubuntu env | grep VAR

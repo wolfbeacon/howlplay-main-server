5e6138514f57adb034f057d5b05791412451b860
David Wan <dave.wan@mail.utoronto.ca>
2018-05-16 16:08:06 -0400

    Finished docker call

--- create-game-server.sh
+++ create-game-server.sh
@@ -4,7 +4,8 @@ IFS=':'
 
 if [ $# -eq 0 ]
 then
-  echo 'Usage: ./create-game-server.sh ENV_VAR_0:value_0 ENV_VAR_1:value_1 ... ENV_VAR_N:value_N';
+  # echo 'Usage: ./create-game-server.sh ENV_VAR_0:value_0 ENV_VAR_1:value_1 ... ENV_VAR_N:value_N';
+  echo 'Usage: ./create-game-server.sh QUIZ_HASH:value_0 ADMIN_KEY:value_1';
   exit 1;
 fi
 
@@ -15,4 +16,6 @@ do
   shift;
 done
 
-docker run --env QUIZ_HASH --env ADMIN_KEY ubuntu env | grep VAR
+# echo $QUIZ_HASH;
+# echo $ADMIN_KEY;
+docker run --env QUIZ_HASH=$QUIZ_HASH --env ADMIN_KEY=$ADMIN_KEY 48d90acd88cc
